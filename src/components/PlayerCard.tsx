843beb7ebda38ebdda8a9d73e5a8e7b2f47428e3
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-10 10:43:34 -0800

    add material UI and icons to project and style teams

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import type { PlayerObject } from '../interfaces/PlayerObject';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 interface Props {
   player: PlayerObject;
@@ -8,13 +16,15 @@ interface Props {
 const PlayerCard = (props: Props) => {
   const { player_name, team_name } = props.player;
   return (
-    <div>
-      <div className="playerList">
-        <div>{player_name}</div>
-        <div>{team_name}</div>
+    <>
+      <TableCell component="th" scope="row">
+        {player_name}
+      </TableCell>
+      <TableCell>{team_name}</TableCell>
+      <TableCell>
         <button>Delete Player</button>
-      </div>
-    </div>
+      </TableCell>
+    </>
   );
 };
 
--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
 import { PlayersContext } from '../data/PlayersContext';
 import PlayerCard from './PlayerCard';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const Players = () => {
   return (
-    <PlayersContext.Consumer>
-      {(players) =>
-        players && (
-          <div>
-            {players.players.map((player) => {
-              return (
+    <>
+      <TableContainer component={Paper}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Player Name</TableCell>
+            <TableCell>Team</TableCell>
+            <TableCell>Remove Player</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <PlayersContext.Consumer>
+            {(players) =>
+              players && (
                 <>
-                  <PlayerCard player={player} />
+                  {players.players.map((player) => {
+                    return (
+                      <TableRow>
+                        <PlayerCard player={player} />
+                      </TableRow>
+                    );
+                  })}
                 </>
-              );
-            })}
-          </div>
-        )
-      }
-    </PlayersContext.Consumer>
+              )
+            }
+          </PlayersContext.Consumer>
+        </TableBody>
+      </TableContainer>
+    </>
   );
 };
 
